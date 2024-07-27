aed0f68b70654fe7c8b8a86cafcc64bc9b990795
Cole Shepherd <cole@coledeanshepherd.com>
2024-07-27 16:57:34 -0700

    Progress

--- NextGenToDoApp/Interpreter.cs
+++ NextGenToDoApp/Interpreter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace NextGenToDoApp;
 
@@ -6,22 +7,22 @@ public static class Interpreter
 {
     public static IJSRuntime JSRuntime = null!;
 
-    public static object? Interpret(string sourceCode)
+    public static async Task<object?> Interpret(string sourceCode)
     {
         var tokens = Lexer.Tokenize(sourceCode);
         var parseTree = Parser.Parse(tokens);
         TypeChecker.CheckType(parseTree);
-        var result = Interpreter.Interpret(parseTree);
+        var result = await Interpret(parseTree);
         return result;
     }
 
-    public static object? Interpret(ParseNode parseNode)
+    public static async Task<object?> Interpret(ParseNode parseNode)
     {
         if (parseNode.ParseNodeType == ParseNodeType.Program)
         {
             foreach (var child in parseNode.Children)
             {
-                Interpret(child);
+                await Interpret(child);
             }
 
             return null;
@@ -31,7 +32,7 @@ public static class Interpreter
             var functionSymbol = parseNode.Children[0].Symbol;
 
             var argNodes = parseNode.Children.Skip(1).Where(c => c.ParseNodeType.IsExpression()).ToList();
-            var args = argNodes.Select(Interpret).ToList();
+            var args = await Task.WhenAll(argNodes.Select(Interpret));
 
             if (functionSymbol == TypeChecker.Symbols["ConsoleLog"])
             {
@@ -52,13 +53,56 @@ public static class Interpreter
                     throw new Exception("SetDocumentTitle expects a string argument");
                 }
 
-                Task.Run(() => JSRuntime.InvokeVoidAsync("setDocumentTitle", s));
+                await JSRuntime.InvokeVoidAsync("setDocumentTitle", s);
                 return null;
+            }
+            else if (functionSymbol == TypeChecker.Symbols["CreateUI"])
+            {
+                var arg = args.Single();
+                //if (arg is not HtmlNode htmlNode)
+                //{
+                //    throw new Exception("CreateUI expects an HtmlNode argument");
+                //}
+
+                await JSRuntime.InvokeVoidAsync("CreateUI", arg);
+                return null;
+            }
+            else if (functionSymbol == TypeChecker.Symbols["h1"])
+            {
+                var arg = args.Single();
+                //if (arg is not List<HtmlNode> htmlNodes)
+                //{
+                //    throw new Exception("h1 expects a list of HtmlNode arguments");
+                //}
+
+                return await JSRuntime.InvokeAsync<ElementReference>("h1", arg);
+            }
+            else if (functionSymbol == TypeChecker.Symbols["text"])
+            {
+                var arg = args.Single();
+                if (arg is not string s)
+                {
+                    throw new Exception("text expects a string argument");
+                }
+
+                return await JSRuntime.InvokeAsync<ElementReference>("text", s);
             }
             else
             {
                 throw new NotImplementedException();
             }
+        }
+        else if (parseNode.ParseNodeType == ParseNodeType.ListLiteral)
+        {
+            System.Collections.ArrayList list = new();
+
+            foreach (var child in parseNode.Children.Where(c => c.ParseNodeType == ParseNodeType.Expression))
+            {
+                var item = await Interpret(child);
+                list.Add(item);
+            }
+
+            return list;
         }
         else if (parseNode.ParseNodeType == ParseNodeType.StringLiteral)
         {
@@ -66,7 +110,7 @@ public static class Interpreter
         }
         else
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException($"Unknown parse node type: {parseNode.ParseNodeType}");
         }
     }
 }
