b809de6ed08e0a9ae73f8e766ca9b6c4e58c5abe
Cole Shepherd <cole@coledeanshepherd.com>
2024-10-11 17:22:07 -0700

    Rework test

--- NextGenToDoApp.Tests/CompilerTests.cs
+++ NextGenToDoApp.Tests/CompilerTests.cs
@@ -5,29 +5,7 @@ public class CompilerTests
     [Fact]
     public void CompilerWorks()
     {
-        string sourceCode =
-@"consoleLog = fn (text: Text): Nothing -> execJs[Nothing](""console.log(text)"")
-
-setDocumentTitle = fn (title: Text): Nothing -> execJs[Nothing](""document.title = title"")
-
-createUI = fn (node: HTMLNode): Nothing -> execJs[Nothing](""document.body.appendChild(node)"")
-
-h1 = fn (text: Text): HTMLNode ->
-	execJs[HTMLNode](
-""const elem = document.createElement('h1');
-for (const child of children) {
-    elem.appendChild(child);
-}
-return elem;""
-	)
-
-txt = fn (text: Text): HTMLNode -> execJs[HTMLNode](""document.createTextNode(text)"")
-
-setDocumentTitle(""Next Gen To-Do App"")
-
-createUI(
-  div([ h1([ txt(""Next Gen To-Do App"") ]) ])
-)";
+        string sourceCode = @"log = fn (text: Text): Nothing -> exec_JS[Nothing](""console.log(text)"")";
         var jsCode = Compiler.Compile(sourceCode);
     }
 }
