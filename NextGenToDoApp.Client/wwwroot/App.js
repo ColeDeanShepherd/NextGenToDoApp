d57895518ffa1a03d46c5f94e8f4b4bf192da92e
Cole Shepherd <cole@coledeanshepherd.com>
2024-10-14 10:14:10 -0700

    Fix adding text node.

--- NextGenToDoApp.Client/wwwroot/App.js
+++ NextGenToDoApp.Client/wwwroot/App.js
@@ -1,6 +1,11 @@
 const set_document_title = (title) => { document.title = title };
 const log = (text) => { console.log(text) };
 const create_UI = (node) => { document.body.appendChild(node) };
-const txt = (text) => { document.createTextNode(text) };
+const txt = (text) => { return document.createTextNode(text); };
+const div = (children) => { const elem = document.createElement('div');
+	for (const child of children) {
+		elem.appendChild(child);
+	}
+	return elem; };
 set_document_title("Next Gen To-Do App")
 create_UI(txt("Next Gen To-Do App"))
